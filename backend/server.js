4c34100f038c23baf92fabaceac65783762ae3c9
Mohammed Mohammed <142446903+Mohammed3ohammed@users.noreply.github.com>
2026-06-01 17:43:09 +0300

    Handle uncaught exceptions and shutdown server safely

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,11 @@
 
 
+process.on('uncaughtException', err => {
+    console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+    console.log(err.name, err.message);
+    process.exit(1);
+});
+
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
     console.log(`App running on port ${port}...`);
