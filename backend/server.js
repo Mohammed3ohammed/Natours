af71e8a771b1d2b3bfe8417a2324af0349d9d491
Mohammed Mohammed <142446903+Mohammed3ohammed@users.noreply.github.com>
2026-06-01 18:06:30 +0300

    Connect to MongoDB using Mongoose and handle connection success

--- backend/server.js
+++ backend/server.js
@@ -8,12 +8,20 @@ process.on('uncaughtException', err => {
 
 dotenv.config({path: './config.env'});
 
+const { default: mongoose } = require('mongoose');
 const app = require('./app');
 
 const DB = process.env.DATABASE.replace(
 '<PASSWORD>',
 process.env.DATABASE_PASSWORD
 );
+
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+})
+.then(() => console.log('DB connection successful'));
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
