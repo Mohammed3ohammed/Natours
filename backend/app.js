b14d0bc8107439963b40524cd8d82454c4c5ca2e
Mohammed Mohammed <142446903+Mohammed3ohammed@users.noreply.github.com>
2026-06-01 21:17:14 +0300

    Add logging, rate limiting, and Stripe webhook handling middleware

--- backend/app.js
+++ backend/app.js
@@ -14,3 +14,22 @@ app.options('*', cors());
 app.use(express.static(Path.join(__dirname, 'public')));
 
 app.use(helmet());
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+const limiter = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: 'Too many requests from this IP, please try again in an hour!'
+});
+
+app.use('/api', limiter);
+
+app.post(
+    '/webhook-checkout',
+    bodyParser.raw({type: 'application/json' }),
+    bookingController.webhookCheckout
+);
+
