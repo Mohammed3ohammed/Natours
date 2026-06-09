4b2c8f3c7add80aa1f410381368dafec683c441b
Mohammed Mohammed <142446903+Mohammed3ohammed@users.noreply.github.com>
2026-06-09 18:04:26 +0300

    Implement user signup and login controllers

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,7 +1,60 @@
 const jwt = require('jsonwebtoken');
 
 const signToken = id => {
-    return jwt.sign({ id }, procces.env.JWT_SECRET, {
-        expiresIn: procces.env.JWT_EXPIRES_IN
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN
     });
 };
+
+const createSendToken = (user, stateusCode, req, res) => {
+    const token = signToken(user._id);
+
+    res.cookie('jwt', token, {
+        expires: new Date(
+            Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+        ),
+        httpOnly: true,
+        secure: req.secure || req.headers['x-forwarded-proto'] === 'https'
+    });
+
+    user.password = undefined;
+
+    res.status(statusCode).json({
+        status: 'success',
+        token,
+        data: {
+            user
+        }
+    });
+};
+
+exports.signup = catchAsync(async (req, res, next) => {
+    const newUser = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        passwordConfirm: req.body.passwordConfirm
+    });
+
+    const url = `${req.ptocol}://${req.get('host')}/me`;
+
+    await new Email(newUser, url).sendWelcome();
+
+    createSendToken(newUser, 201, req, res);
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+    const { email, password} = req.body;
+
+    if (!email || !password) {
+        return next(new AppError('Please provide email and password!', 400));
+    }
+
+    const user = await User.findOne({ email }).select('+password');
+
+    if (!user || !(await user.correctPassword(password, user.password))) {
+        return next(new AppError('Incorrect email or password', 401));
+    }
+
+    createSendToken(user, 200, req, res);
+});
