790572cdc20c7cea6ec7bdb8f836ab6f78968039
Mohammed Mohammed <142446903+Mohammed3ohammed@users.noreply.github.com>
2026-06-09 18:09:49 +0300

    Add logout controller to clear JWT cookie

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -58,3 +58,12 @@ exports.login = catchAsync(async (req, res, next) => {
 
     createSendToken(user, 200, req, res);
 });
+
+exports.logout = (req, res) => {
+    res.cookie('jwt', 'loggedout', {
+        expires: new Date(Date.now() + 10 * 1000),
+        httpOnly: true
+    });
+
+    res.status(200).json({ status: 'success'});
+};
