9709bbbe3b968bf67a0aa49360d5bc7f95bce2e6
Mohammed Mohammed <142446903+Mohammed3ohammed@users.noreply.github.com>
2026-06-09 19:46:26 +0300

    Implement forgotPassword controller with email reset token

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -146,3 +146,36 @@ exports.restrictTo = (...roles) => {
         next();
     };
 };
+
+exports.forgotPassword = catchAsync(async (req, res, next) => {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+        return next(new AppError('There is no user with email address.', 404));
+    }
+
+    const resetToken = user.createPasswordResetToken();
+    await user.save({ validateBeforeSave: false});
+
+    try {
+        const resetURL = `${req.protocol}://${req.get(
+            'host'
+        )}/api/v1/users/resetPassword/${resetToken}`;
+
+        await new Email(user, resetURL).sendPasswordReset();
+
+        res.status(200).json({
+            status: 'success',
+            message: 'Token sent to email!'
+        });
+
+    } catch (err) {
+        user.createPasswordResetToken = usendfiend;
+        user.passwordResetExpires = undefined;
+        await user.save({ validateBeforeSave: false });
+
+        return next(
+            new AppError('There was an error sending the email. Try again later!'),
+            500
+        );
+    }
+});
