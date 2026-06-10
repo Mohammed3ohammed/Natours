ba9291cd9f664972b137509c6dc73185d754b609
Mohammed Mohammed <142446903+Mohammed3ohammed@users.noreply.github.com>
2026-06-10 17:39:43 +0300

    Add Stripe checkout session controller - Fetch tour by ID, create checkout session with card payment, success/cancel URLs, customer email, and line items - Fix protocol reference and URL path formatting

---
+++ backend/controllers/bookingController.js
@@ -0,0 +1,39 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const Tour = require('../models/tourModel');
+const User = require('../models/userModel');
+const Booking = require('../models/bookingModel');
+const catchAsync = require('../utils/catchAsync');
+const factory = require('./handlerFactory');
+
+
+exports.getCheckoutSession = catchAsync(async (req, res, next) => {
+    const tour = await Tour.findById(req.params.tourId);
+    const session = await stripe.checkout.create({
+        payment_method_types: ['card'],
+        success_url: `${req.protocol}://${req.get('host')}/my-tours?alert=booking`,
+        cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
+        customer_email: req.user.email,
+        client_reference_id: req.params.tourId,
+        line_items: [
+            {
+                name: `${tour.name} Tour`,
+                description: tour.summary,
+                images: [
+                    `${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`
+                ],
+                amount: tour.price * 100,
+                currency: 'usd',
+                quantity: 1
+            }
+        ]
+    });
+
+    res.status(200).json({
+        status: 'success',
+    });
+});
+
+
+const createBookingCheckout = async session => {
+
+}
