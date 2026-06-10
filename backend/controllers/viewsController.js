e0fcce1495698c68c35a3891f5926438ec79b444
Mohammed Mohammed <142446903+Mohammed3ohammed@users.noreply.github.com>
2026-06-10 21:11:59 +0300

    Add getOverview controller - Fetch all tours from database and render overview template with tours data

--- backend/controllers/viewsController.js
+++ backend/controllers/viewsController.js
@@ -12,3 +12,11 @@ exports.alerts = (req, res, next) => {
          next();
 };
 
+exports.getOverview = catchAsync(async (req, res, next) => {
+    const tours = await Tour.find();
+
+    res.status(200).render('overview', {
+        title: 'All Tours',
+        tours
+    });
+});
