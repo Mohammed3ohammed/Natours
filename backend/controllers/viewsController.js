1f07561683e5a56dc65d117064c8e92a5a87ecaf
Mohammed Mohammed <142446903+Mohammed3ohammed@users.noreply.github.com>
2026-06-10 21:27:30 +0300

    Add getTour controller - Fetch tour by slug, populate reviews with selected fields, handle 404 error if not found, and render tour template

--- backend/controllers/viewsController.js
+++ backend/controllers/viewsController.js
@@ -20,3 +20,19 @@ exports.getOverview = catchAsync(async (req, res, next) => {
         tours
     });
 });
+
+exports.getTour = catchAsync(async (req, res, next) => {
+    const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+        path: 'reviews',
+        fields: 'review rating user'
+    });
+
+    if (!tour) {
+        return next(new AppError('There is not tour with that name.', 404));
+    }
+
+    res.status(200).render('tour', {
+        title: `${tour.name} Tour`,
+        tour
+    });
+});
