36aff5e8e961d58fbaca1b1beb99a760154f91a8
Mohammed Mohammed <142446903+Mohammed3ohammed@users.noreply.github.com>
2026-06-11 15:36:43 +0300

    Add custom error handlers for MongoDB - Handle cast errors, duplicate field errors, and validation errors with descriptive AppError messages

---
+++ backend/controllers/errorController.js
@@ -0,0 +1,20 @@
+const AppError = requier('./../utils/appError');
+
+const handleCastErrorDB = err => {
+    const message = `Invalid ${err.path}: ${err.value}.`;
+    return new AppError(message, 400)
+};
+
+const handleDuplicateFieldsDB = err => {
+    const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+    const message = `Duplicate field value: ${value}. Please use another value!`;
+    return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = err => {
+    const errors = Object.values(err.errors).map(el => el.message);
+
+    const message = `Invalid input data. ${errors.join('. ')}`;
+    return new AppError(message, 400);
+};
+
